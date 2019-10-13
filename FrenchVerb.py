9865f22f59d4c28214103f904636306defeed80b
wiqipedia <56439386+wiqipedia@users.noreply.github.com>
2019-10-13 22:45:00 +0800

    Major update to FrenchVerb.py
    Revamped approach to conjugation - Using dictionaries and for loop to minimize redundancy.
    Added function to select tense as well (currently only present and future available).

--- FrenchVerb.py
+++ FrenchVerb.py
@@ -1,53 +1,31 @@
+pronouns = ['Je','Tu','Il/Elle','Nous','Vous','Ils/Elles']
+tenses = ['present','future']
+endings = {
+    'er':{
+        'present':['e','es','e','ons','ez','ent'],
+        'future':['erai','eras','era','erons','erez','eront']
+    },
+    'ir':{
+        'present':['is','is','it','issons','issez','issent'],
+        'future':['irai','iras','ira','irons','irez','iront']
+    },
+    're':{
+        'present':['s','s','','ons','ez','ent'],
+        'future':['rai','ras','ra','rons','rez','ront']
+    }
+}
+
 class FrenchVerb:
-    def __init__ (self, verb_in):
+    def __init__ (self, verb_in,tense):
         self.verb_in = verb_in
-
-        if verb_in[-2:] == 'er':
-            if verb_in == 'aller':
-                fpc = 'vais'
-                spc = 'vas'
-                tpc = 'va'
-                fppc = 'allons'
-                sppc = 'allez'
-                tppc = 'vont'
-
-            else:
-                verb_stem = verb_in[:-2]
-                fpc = verb_stem + 'e'
-                spc = verb_stem + 'es'
-                tpc = verb_stem + 'e'
-                fppc = verb_stem + 'ons'
-                sppc = verb_stem + 'ez'
-                tppc = verb_stem + 'ent'
-
-        elif verb_in[-2:] == 'ir':
-            verb_stem = verb_in[:-2]
-            fpc = verb_stem + 'is'
-            spc = verb_stem + 'is'
-            tpc = verb_stem + 'it'
-            fppc = verb_stem + 'issons'
-            sppc = verb_stem + 'issez'
-            tppc = verb_stem + 'issent'
-
-        else:
-            verb_stem = verb_in[:-2]
-            fpc = verb_stem + 's'
-            spc = verb_stem + 's'
-            tpc = verb_stem  
-            fppc = verb_stem + 'ons'
-            sppc = verb_stem + 'ez'
-            tppc = verb_stem + 'ent'
-
-        if verb_in[0] == 'a' or 'e' or 'i' or 'o' or 'u':
-            print ("J'" + fpc)
-        else:
-            print("Je " + fpc)
-
-        print("Tu " + spc)
-        print("Il/Elle " + tpc)
-        print("Nous " + fppc)
-        print("Vous " + sppc)
-        print("Ils " + tppc)
+        verb_stem = verb_in[:-2]
+        verb_group = verb_in[-2:]
+        if verb_in[0]=='a' or 'e' or 'i' or 'o' or 'u' or 'h':
+                pronouns[0]="J'"
+        for p in pronouns:
+            verb_conjugated = verb_stem + endings[verb_group][tense][pronouns.index(p)]
+            print(p + ' ' + verb_conjugated)
 
 verb_in = input('Tapez un verbe francais svp: ')
-conj = FrenchVerb(verb_in)
+tense = input('Choisissez entre present et future: ')
+conj = FrenchVerb(verb_in,tense)
